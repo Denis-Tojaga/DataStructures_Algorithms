89d39aaacb154a331762cb6be4c750d1fc94a943
Denis <denis.tojaga@edu.fit.ba>
2021-01-23 20:17:14 +0100

    //bugFix

--- PrirorityQueueLinked.h
+++ PrirorityQueueLinked.h
@@ -11,6 +11,7 @@ class PrirorityQueueLinked : public Queue<T>
 public:
 	bool IsEmpty() { return _first == nullptr; }
 	bool IsFull() { return false; }
+	int GetCounter() { return _counter; }
 	PrirorityQueueLinked(): _first(nullptr),_counter(0){}
 
 	void Add_To_Queue(T value)
@@ -31,12 +32,29 @@ public:
 			_first = newNode;
 		cout << "\nElement -> | " << newNode->value << " | is added to priority queue.";
 	}
-
 	T Remove_From_Queue()
 	{
 		if (IsEmpty())
 			ShowException();
+		Node<T>* tempNode = _first;
+		T deletedValue = tempNode->value;
+		_first = _first->_nextNode;
+		delete tempNode;
+		_counter--;
+		cout << "\nElement -> | " << deletedValue << " | is removed from priority queue.";
 	}
 
+	T operator[](int index)
+	{
+		if (index<0 || index>_counter)
+			throw exception("Value of index input is not valid!\n");
+		Node<T>* tempNode = _first;
+		for (int i = 0; i < index; i++)
+			tempNode = tempNode->_nextNode;
+		return tempNode->value;
+	}
+	void Print()
+	{
 
+	}
 };
