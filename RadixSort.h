4e9486e0ae8df494110aba3bab6dfc4d1f2ed0a3
Denis <denis.tojaga@edu.fit.ba>
2021-01-26 14:47:05 +0100

    //radixSort implementation finished

--- RadixSort.h
+++ RadixSort.h
@@ -3,6 +3,61 @@
 using namespace std;
 
 
+
+void RecursiveFunction(int* _array, int size, int exponent)
+{
+	//Create one array where the all possible digits can show ( from 0 to 9)
+	int _counterArray[10] = { 0 };
+	//Create one helper array to place element in correct order
+	int* helperArray = new int[size];
+
+
+	///First fase - To count all numbers with last digit in _counterArray
+	for (int i = 0; i < size; i++)
+	{
+		int element = _array[i];
+		int lastDigit = (element / exponent) % 10;
+		_counterArray[lastDigit]++;
+	}
+
+
+	//Second fase is to add all counters in _counterArray one by one
+	for (int i = 1; i < 10; i++)
+		_counterArray[i] += _counterArray[i - 1];
+
+
+	//Third fase is to go from behind and take the element's last digit, go to its location in _counterArray and subtract the number on that location
+	//with  -1, after doing that we will get location to place that element in third array (helperArray)
+	for (int i = size -  1 ; i >= 0; i--)
+	{
+		int element = _array[i];
+		int lastDigit = (element / exponent) % 10;
+
+		int& helperCounter = _counterArray[lastDigit];
+		helperCounter--;
+		helperArray[helperCounter] = element;
+	}
+
+
+
+	//Fourth fase is to move all elements from helperArray to main array(after finishing all this for all digits)
+	for (int i = 0; i < size; i++)
+		_array[i] = helperArray[i];
+}
+
+int GetMaxValue(int* _array, int size)
+{
+	int maxValue = INT_MIN;
+	for (int i = 0; i < size; i++)
+		if (_array[i] > maxValue)
+			maxValue = _array[i];
+
+	return maxValue;
+}
+
+
+
+
 void RadixSort()
 {
 	//---------Creation of the array -----------------
@@ -17,9 +72,21 @@ void RadixSort()
 	//-------------------------------------------
 
 
+	int maxValue = GetMaxValue(_array, size);
+	int exponent = 1;
+	while (maxValue > 0)
+	{
+		RecursiveFunction(_array, size, exponent);
+		exponent = exponent * 10;
+		maxValue = maxValue / 10;
+	}
 
 
 
+	
+	cout << "\n\n\n *** RADIX SORT ALGORITHM *** \n\n";
+	for (int i = 0; i < size; i++)
+		cout << _array[i] << " | ";
 
-
+	cout << "\n\n";
 }
