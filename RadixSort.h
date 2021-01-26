38aa6f16c630af1dd5f5907b9462c8f765bfd463
Denis <denis.tojaga@edu.fit.ba>
2021-01-26 14:27:47 +0100

    //implementation RadixSort

--- RadixSort.h
+++ RadixSort.h
@@ -5,5 +5,21 @@ using namespace std;
 
 void RadixSort()
 {
+	//---------Creation of the array -----------------
+	int _array[10], size = 10;
+	//Generates 10 random numbers and adds them to array;
+	for (int i = 0; i < 10; i++)
+		_array[i] = rand() % 100 + 1;
+
+	cout << "\n *** Unsorted array *** \n\n";
+	for (int i = 0; i < 10; i++)
+		cout << _array[i] << " | ";
+	//-------------------------------------------
+
+
+
+
+
+
 
 }
