b1c9853586abaede981c43c49e966e861ce6e35e
Denis <denis.tojaga@edu.fit.ba>
2021-01-29 14:42:30 +0100

    //QuickSort implementation

--- QuickSort.h
+++ QuickSort.h
@@ -4,9 +4,42 @@ using namespace std;
 
 
 
-int Partition(int array[],int low,int high)
+int Partition(int array[],const int low,const int high)
 {
+	int pivotElement = array[low];
+	int i = low, j = high;
+	while (i < j)
+	{
+		do
+		{
+			i++;
+		} while (array[i]<=pivotElement);
 
+		do
+		{
+			j--;
+		} while (array[j]>pivotElement);
+
+		if (i < j)
+		{
+			int tempValue = array[i];
+			array[i]= 
+		}
+	}
+
+}
+
+
+void Quick(int array[],const int low,const int high)
+{
+	int locationOfPartition;
+	//makes sure that there are minimum 2 elements in the array
+	if (low < high)
+	{
+		locationOfPartition = Partition(array, low, high);
+		Quick(array, low, locationOfPartition);//recursive call for left side
+		Quick(array, locationOfPartition + 1, high);//recursive call for right side
+	}
 }
 
 
