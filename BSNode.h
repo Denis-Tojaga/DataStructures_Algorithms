a4c03db9ed6d814aee7b2536e673f8f065c22da6
Denis <denis.tojaga@edu.fit.ba>
2021-02-08 19:15:12 +0100

    //Update

--- BSNode.h
+++ BSNode.h
@@ -1,1 +1,17 @@
 #pragma once
+
+
+template<class Key>
+class BSNode
+{
+public:
+	Key value;
+	BSNode* leftChild;
+	BSNode* rightChild;
+	BSNode(Key keyValue) : value(keyValue)
+	{
+		leftChild = nullptr;
+		rightChild = nullptr;
+	}
+
+};
