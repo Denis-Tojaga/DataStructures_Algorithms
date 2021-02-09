eb00f60f00e5974dfff02d611ef4693e6e2e5750
Denis <denis.tojaga@edu.fit.ba>
2021-02-09 11:52:29 +0100

    //Update

--- BSTraverse.h
+++ BSTraverse.h
@@ -9,4 +9,53 @@ template<class Key>
 class BSTraverse
 {
 
+#pragma region Recursion Traversing
+
+	//PostOrder traversing (left child, right child, value comes last)
+	static void Recursion_PostOrder(BSNode<Key>* node, List<Key>* listOfNodes)
+	{
+		if (node != nullptr)
+		{
+			Recursion_PostOrder(node->leftChild, listOfNodes);
+			Recursion_PostOrder(node->rightChild, listOfNodes);
+			listOfNodes->Add_On_End(node->value);
+		}
+	}
+
+	//InOrder traversing(left child,value,right child comes last)
+	static void Recursion_InOrder(BSNode<Key>* node, List<Key>* listOfNodes)
+	{
+		if (node != nullptr)
+		{
+			Recursion_InOrder(node->leftChild, listOfNodes);
+			listOfNodes->Add_On_End(node->value);
+			Recursion_InOrder(node->rightChild, listOfNodes);
+		}
+	}
+
+	//PreOrder traversing(value comes first,left child, right child)
+	static void Recursion_PreOrder(BSNode<Key>* node, List<Key>* listOfNodes)
+	{
+		if (node != nullptr)
+		{
+			listOfNodes->Add_On_End(node->value);
+			Recursion_PreOrder(node->leftChild, listOfNodes);
+			Recursion_PreOrder(node->rightChild, listOfNodes);
+		}
+	}
+
+#pragma endregion
+
+
+public:
+
+	//Three recursions
+
+
+	//Three itertive methods
+
+
+	//level by level iterative
+
+
 };
