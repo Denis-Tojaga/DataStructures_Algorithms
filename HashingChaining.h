a162dfdf5fb4369ad9f963e1afbbce724a47a452
Denis <denis.tojaga@edu.fit.ba>
2021-02-17 17:00:54 +0100

    //HashingChaining implemented

--- HashingChaining.h
+++ HashingChaining.h
@@ -5,3 +5,50 @@
 #include "LinkedList.h";
 
 using namespace std;
+
+
+template<class T>
+class HashingChaining : public Hashing<T>
+{
+	LinkedList<T>* _array;
+	int _numberOfElements;
+
+	int HashFunction(T value)
+	{
+		return value % _numberOfElements;
+	}
+
+public:
+	HashingChaining(int numberOfElements = 10)
+	{
+		_numberOfElements = numberOfElements;
+		_array = new LinkedList<T>[_numberOfElements];
+	}
+
+
+	void Add(T value)
+	{
+		int location = HashFunction(value);
+		LinkedList<T>& list = _array[location];
+		list.Add_On_End(value);
+	}
+
+	void Remove(T value)
+	{
+		int location = HashFunction(value);
+		LinkedList<T>& list = _array[location];
+		T removed = list.Remove_By_Key(value);
+		cout << "Element -> " << removed << " removed from HashSet!\n";
+	}
+
+
+	void Print()
+	{
+		for (int i = 0; i < _numberOfElements; i++)
+		{
+			cout < "\nList on location -> " << i << "\n";
+			cout << _array[i].Print() << endl;
+		}
+	}
+
+};
